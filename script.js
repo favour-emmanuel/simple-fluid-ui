139b2bc194e9c16944a12d7ca8eeaf77f516bebe
Your Name <iyenefavour@gmail.com>
2023-10-17 10:19:19 +0100

    first save

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const div = document.getElementsByClassName('alert')[0];
+function alertDiv() {
+    alert()
+    
+}
+
+function closeBtn() {
+    const close = document.getElementById('alert');
+    close.style.display = ('none');
+}
+
+
+const close = document.getElementById('close');
+close.style.backgroundColor = ('red');
+close.style.color = ('white');
+close.style.padding = ('5px 15px');
+close.style.border = ('none');
+close.style.margin = ('10px');
+close.style.margin = ('10px');
+
+
+
+const tourBtn = document.getElementById('tour');
+tourBtn.style.backgroundColor = ('#0fffaf');
+tourBtn.style.color = ('black');
+tourBtn.style.padding = ('5px');
+tourBtn.style.border = ('none');
+close.style.margin = ('10px');
+
+
+function openAlert() {
+    alert('welcome to our Page!')
+}
+
